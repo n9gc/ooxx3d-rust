4b7caecf818c6a5541d5857e5c3d5b59bf467c3d
E0SelmY4V <yabo1887415157@qq.com>
2024-06-13 01:05:46 +0800

    add: 有色输出

--- n9gc_ooxx/src/text_out.rs
+++ n9gc_ooxx/src/text_out.rs
@@ -3,6 +3,7 @@
 //! 用来在命令行里输出游戏对局的模块。
 
 use crate::{GameInfo, OperationInfo, Piece};
+use crossterm::style::Stylize;
 
 pub struct PieceStr {
     you: String,
@@ -23,30 +24,52 @@ impl Outer {
             Piece::None => &self.piece_str.none,
         }
     }
+    fn out(&self, info: &str, high: bool) {
+        print!(
+            "{}",
+            if high {
+                info.white().on_dark_red()
+            } else {
+                info.reset()
+            }
+        );
+    }
+    fn voidline(&self, game_info: &GameInfo) {
+        for _j in 0..(2 * game_info.size + 10) {
+            self.out(" ", false);
+        }
+		self.out("\n", false);
+    }
     fn line(&self, game_info: &GameInfo) {
-        std::print!("╬");
+        self.out(" ╬", false);
         for _j in 0..game_info.size {
-            std::print!("═══╬");
+            self.out("═══╬", false);
         }
     }
     pub fn out_board(&self, game_info: &GameInfo, operation_info: &OperationInfo) {
         let size = game_info.size;
         let board = &game_info.board;
         let cursor = operation_info.cursor;
+		self.voidline(game_info);
         for i in 0..size {
-			self.line(game_info);
-        	std::print!(" \n║ ");
+            self.line(game_info);
+            self.out(" \n ║ ", false);
             for j in 0..size {
+                let now_piece = self.piece(&board[i][j]);
                 if (i, j) == cursor {
-                    std::print!("{}", self.piece(&board[i][j]));
+                    self.out(now_piece, true);
                 } else {
-                    std::print!("{}", self.piece(&board[i][j]));
+                    self.out(now_piece, false);
                 }
-                std::print!("║ ");
+                self.out("║ ", false);
             }
-            std::print!("\n");
+            self.out("\n", false);
         }
-		self.line(game_info);
+        self.line(game_info);
+        self.out(" \n", false);
+		self.voidline(game_info);
+		let str = "";
+		print!("{}", str.reset());
     }
 }
 
@@ -56,24 +79,32 @@ mod test {
 
     use super::{Outer, PieceStr};
 
-	#[test]
-	pub fn out_borad() {
-		let game_info = GameInfo {
-			board: vec![
-				vec![Piece::You, Piece::Me, Piece::None],
-				vec![Piece::Me, Piece::You, Piece::None],
-				vec![Piece::You, Piece::None, Piece::Me],
-			],
-			size: 3,
-		};
-		let operation_info = OperationInfo {
-			cursor: (2, 3),
-		};
-		let outer = Outer::new(PieceStr {
-			you: String::from("OO"),
-			me: String::from("XX"),
-			none: String::from("  "),
-		});
-		outer.out_board(&game_info, &operation_info);
-	}
+    #[test]
+    pub fn out_borad() {
+        let game_info_0 = GameInfo {
+            board: vec![
+                vec![Piece::You, Piece::Me, Piece::None],
+                vec![Piece::Me, Piece::You, Piece::None],
+                vec![Piece::You, Piece::None, Piece::Me],
+            ],
+            size: 3,
+        };
+        let game_info_1 = GameInfo {
+            board: vec![
+                vec![Piece::You, Piece::Me, Piece::None, Piece::You],
+                vec![Piece::You, Piece::Me, Piece::None, Piece::Me],
+                vec![Piece::Me, Piece::You, Piece::None, Piece::None],
+                vec![Piece::You, Piece::None, Piece::Me, Piece::Me],
+            ],
+            size: 4,
+        };
+        let outer = Outer::new(PieceStr {
+            you: String::from("OO"),
+            me: String::from("XX"),
+            none: String::from("  "),
+        });
+        outer.out_board(&game_info_0, &OperationInfo { cursor: (1, 0) });
+		print!("\n");
+        outer.out_board(&game_info_1, &OperationInfo { cursor: (3, 2) });
+    }
 }
